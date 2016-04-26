eb4ef7bb8e3dec58391bdef8f9cf51fd4757ff94
Ivam Luz <iluz@ciandt.com>
2016-04-26 20:51:36 -0300

    Adjust menu items

--- app/src/main/java/ivamluz/marvelshelf/MainActivity.java
+++ app/src/main/java/ivamluz/marvelshelf/MainActivity.java
@@ -80,17 +80,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_bookmarks) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_characters_all) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_characters_seen) {
 
         }
 
