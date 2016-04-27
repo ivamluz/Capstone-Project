fedc783fed0fcb2f760f8f651b8a2f7b0c370037
Ivam Luz <iluz@ciandt.com>
2016-04-26 23:16:34 -0300

    Apply Android Studio auto formatting

--- app/src/main/java/ivamluz/marvelshelf/activities/MainActivity.java
+++ app/src/main/java/ivamluz/marvelshelf/activities/MainActivity.java
@@ -2,11 +2,10 @@ package ivamluz.marvelshelf.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import ivamluz.marvelshelf.R;
 import ivamluz.marvelshelf.fragments.AllCharactersFragment;
